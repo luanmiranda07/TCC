9611df15b49489d1426151da2107f5952e772a8f
Luan Miranda <luan.miranda@caiquecastro.adv.br>
2026-04-10 16:50:58 -0300

    banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=50)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    codigo_barras = models.CharField(max_length=50, unique=True)
+
+class Fornecedor(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
