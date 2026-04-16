db2140761ab87e96470a85398dd9176cb41964df
Luan Miranda <luan.miranda@caiquecastro.adv.br>
2026-04-16 11:56:58 -0300

    admin com html funcionando

--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Produto(models.Model):
@@ -8,6 +9,7 @@ class Produto(models.Model):
     quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     codigo_barras = models.CharField(max_length=50, unique=True)
+    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
 class Fornecedor(models.Model):
     nome = models.CharField(max_length=100)
--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from galeria.models import Produto
 
 
 def login(request):
     return render(request, 'login.html')
 
 def index(request):
-    return render(request, 'index.html')
+    produtos = Produto.objects.all()
+    dados = {
+        'produtos': produtos
+    }
+    return render(request, 'index.html', dados)
